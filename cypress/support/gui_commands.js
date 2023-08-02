15bba19458f5667424e2d5ab195050c8c879a14c
Rafael Grassi Ribon <rafael.ribon@izio.com.br>
2023-08-01 21:41:46 -0300

    Teste da Funcionalidade de label e Milestone

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,3 +1,6 @@
+/// <reference types="Cypress" />
+
+
 Cypress.Commands.add('login', (
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
@@ -53,4 +56,15 @@ Cypress.Commands.add('gui_createIssue', issue => {
   cy.get('.qa-issuable-form-title').type(issue.title)
   cy.get('.qa-issuable-form-description').type(issue.description)
   cy.contains('Submit issue').click()
+})
+
+Cypress.Commands.add('gui_setLabelOnIssue', label => {
+  cy.get('.qa-edit-link-labels').click()
+  cy.contains(label.name).click()
+  cy.get('body').click()
+})
+
+Cypress.Commands.add('gui_setMilestoneOnIssue', milestone => {
+  cy.get('block.milestone .edit-link').click()
+  cy.contains(milestone.title).click()
 })
